5b7308cd75b15529f77f960adad2f1c7c7f54ec4
TickyNucker-Git <84568935+TickyNucker-Git@users.noreply.github.com>
2021-10-12 05:12:03 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module("myApp", []);
+app.controller('myCtrl', function($scope, $http) {
+
+  // users
+  $http.get('https://jsonplaceholder.typicode.com/users')
+    .then(function (response) {
+      $scope.users = response.data;
+    });
+   
+  //todos
+  $scope.todoUrl = "https://jsonplaceholder.typicode.com/todos";
+  $scope.selectedUser = "";
+  $scope.userId = "";
+
+  $scope.myFunc = function() {
+    $scope.userId = $scope.selectedUser.id;
+
+    $http.get($scope.todoUrl + "?userId=" + $scope.userId)
+    .then(function (response) {
+      $scope.todos = response.data;
+    });
+  };
+  //console.log($scope.userId);
+
+  // $http.get('https://jsonplaceholder.typicode.com/todos')
+  //   .then(function (response) {
+  //     $scope.todos = response.data;
+  //   });
+
+});
